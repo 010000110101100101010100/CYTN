c98bfb1dfcf0a053adedc1d721655ad6d1101692
010000110101100101010100 <cryptoken@hotmail.com>
2020-06-23 17:04:38 +0100

    Adjusted Max Connections and Added Nodes.
    Adjusted Max Connections and Added Nodes in order to help with connectivity.

--- src/net.cpp
+++ src/net.cpp
@@ -25,7 +25,7 @@
 using namespace std;
 using namespace boost;
 
-static const int MAX_OUTBOUND_CONNECTIONS = 16;
+static const int MAX_OUTBOUND_CONNECTIONS = 32;
 
 void ThreadMessageHandler2(void* parg);
 void ThreadSocketHandler2(void* parg);
@@ -1233,6 +1233,17 @@ void MapPort()
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
     {"CrYpTo", "52.88.93.148"},
+    {"CrYpTo", "10.100.201.177"},
+    {"CrYpTo", "104.172.24.79"},
+    {"CrYpTo", "86.82.30.234"},
+    {"CrYpTo", "82.134.214.92"},
+    {"CrYpTo", "188.134.72.213"},
+    {"CrYpTo", "198.50.200.139"},
+    {"CrYpTo", "5.9.36.211"},
+    {"CrYpTo", "95.46.199.104"},
+    {"CrYpTo", "23.129.64.12"},
+    {"CrYpTo", "77.99.174.201"},
+    {"CrYpTo", "86.24.26.19"},
 
 };
 
@@ -1304,14 +1315,7 @@ void ThreadDNSAddressSeed2(void* parg)
 
 unsigned int pnSeed[] =
 {
-    0xdf4bd379, 0x7934d29b, 0x26bc02ad, 0x7ab743ad, 0x0ab3a7bc,
-    0x375ab5bc, 0xc90b1617, 0x5352fd17, 0x5efc6c18, 0xccdc7d18,
-    0x443d9118, 0x84031b18, 0x347c1e18, 0x86512418, 0xfcfe9031,
-    0xdb5eb936, 0xef8d2e3a, 0xcf51f23c, 0x18ab663e, 0x36e0df40,
-    0xde48b641, 0xad3e4e41, 0xd0f32b44, 0x09733b44, 0x6a51f545,
-    0xe593ef48, 0xc5f5ef48, 0x96f4f148, 0xd354d34a, 0x36206f4c,
-    0xceefe953, 0x50468c55, 0x89d38d55, 0x65e61a5a, 0x16b1b95d,
-    0x702b135e, 0x0f57245e, 0xdaab5f5f, 0xba15ef63,
+
 };
 
 void DumpAddresses()
